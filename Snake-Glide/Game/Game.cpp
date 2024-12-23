fa6438b763d9698d5a1d6c0df4bb19082482eb63
MuhammadAbuBakarQureshi <152699100+MuhammadAbuBakarQureshi@users.noreply.github.com>
2024-12-23 19:03:38 +0500

    Update Game.cpp

--- Snake-Glide/Game/Game.cpp
+++ Snake-Glide/Game/Game.cpp
@@ -1,12 +1,11 @@
 #include "Game.h"
-
-#include "../Snake/Snake.h"
+//
+//#include "../Snake/Snake.h"
 #include "../Target/Target.h"
-
-using namespace std;
+//
+//using namespace std;
 
 Target target_obj;
-
 
 void Game::generateTarget()
 {
@@ -14,17 +13,17 @@ void Game::generateTarget()
 
     target.setPosition(target_pos);
 
-    cout << "Score : " << Snake::score << endl;
+    score++;
 }
 
 void Game::initVariables()
 {
-    this->window = nullptr;
+    //this->window = nullptr;
 
     // Init Window size
 
-    this->video_mode.height = 800;
-    this->video_mode.width = 1000;
+    //this->video_mode.height = 800;
+    //this->video_mode.width = 1000;
 
     // Creating target
 
@@ -37,11 +36,8 @@ void Game::initVariables()
     generateTarget();
 
     main_menu = false;
-}
 
-void Game::initWindow()
-{
-    this->window = new sf::RenderWindow(this->video_mode, "Snake Game", sf::Style::Close);
+    score = 0;
 }
 
 void Game::initSnake()
@@ -64,13 +60,13 @@ void Game::initSnake()
 
     snake.at(3).setFillColor(sf::Color(255, 165, 0));
 
-    float head = 100;
+    float head = 70;
     float speed = 10;
 
     snake.at(0).setPosition(head, 100);
     snake.at(1).setPosition(head + speed, 100);
-    snake.at(2).setPosition(head + speed, 100);
-    snake.at(3).setPosition(head + speed, 100);
+    snake.at(2).setPosition(head + speed + speed, 100);
+    snake.at(3).setPosition(head + speed + speed + speed, 100);
 
     snake_pos.push_back(snake.at(0).getPosition());
     snake_pos.push_back(snake.at(1).getPosition());
@@ -78,45 +74,24 @@ void Game::initSnake()
     snake_pos.push_back(snake.at(3).getPosition());
 }
 
-// Constructor
+//// Constructor
 
-Game::Game()
+Game::Game(sf::VideoMode video_mode)
 {
+    this->video_mode = video_mode;
+
     initVariables();
-    initWindow();
+    //initWindow();
     initSnake();
 }
+//
+//// Destructor
 
-// Destructor
 
-Game::~Game()
-{
-    delete this->window;
-}
-
-// Accessor
-
-const bool Game::isRunning() const
-{
-
-    return this->window->isOpen();
-}
-
-void Game::isKilledInGame(bool is_crashed)
-{
-    if (is_crashed == true) {
-
-        this_thread::sleep_for(chrono::seconds(1));
-        this->window->close();
-        cout << "Snake is killed" << endl;
-    }
-}
-
-// Snake Data
 
 // Game
 
-void Game::keyboardInput()
+void Game::keyboardInput(sf::RenderWindow* window)
 {
 
     Snake snake_movement;
@@ -152,24 +127,23 @@ void Game::keyboardInput()
 
     // checks if it is hit by wall or itself
 
-    isKilledInGame(snake_movement.isKilledByItself(snake, snake_pos));
+    //isKilledInGame(snake_movement.isKilledByItself(snake, snake_pos), window);
 
-    isKilledInGame(snake_movement.isKilledByWall(snake_pos, video_mode));
-
+    //isKilledInGame(snake_movement.isKilledByWall(snake_pos, video_mode), window);
 }
 
 // Poll Event
 
-void Game::pollEvent()
+void Game::pollEvent(sf::RenderWindow* window)
 {
-    while (this->window->pollEvent(evnt)) {
+    while (window->pollEvent(evnt)) {
 
         switch (evnt.type)
         {
         case sf::Event::Closed:
 
             cout << "Closing Window" << endl;
-            this->window->close();
+            window->close();
             break;
 
         case sf::Event::KeyPressed:
@@ -177,7 +151,7 @@ void Game::pollEvent()
             if (evnt.key.code == sf::Keyboard::Escape) {
 
                 cout << "Closing Window" << endl;
-                this->window->close();
+                window->close();
             }
             break;
         }
@@ -186,50 +160,71 @@ void Game::pollEvent()
 
 // Update
 
-void Game::updateTarget()
+void Game::updateTarget(sf::RenderWindow* window)
 {
 
 }
 
-void Game::updateSnake(){
+void Game::updateSnake(sf::RenderWindow* window){
 
-    this->keyboardInput();
+    this->keyboardInput(window);
 }
 
-void Game::update()
+void Game::update(sf::RenderWindow* window)
 {
 
-    this->pollEvent();
+    this->pollEvent(window);
 
-    updateSnake();
+    updateSnake(window);
 
-    updateTarget();
+    updateTarget(window);
 }
 
 // Render
 
-void Game::renderTarget()
+void Game::renderTarget(sf::RenderWindow* window)
 {
 
-    this->window->draw(target);
+    window->draw(target);
 }
 
-void Game::renderSnake() {
+void Game::renderSnake(sf::RenderWindow* window) {
 
     for (auto& s : snake) {
 
-        this->window->draw(s);
+        window->draw(s);
     }
 }
 
-void Game::render()
+void Game::render(sf::RenderWindow* window)
 {
 
-    this->window->clear(sf::Color(0, 31, 63));
+    window->clear(sf::Color(0, 31, 63));
 
-    renderSnake();
+    renderSnake(window);
     
-    renderTarget();
+    renderTarget(window);
 
-    this->window->display();
+    window->display();
+}
+
+void Game::run(sf::RenderWindow* window, sf::Event evnt)
+{
+    this->evnt = evnt;
+
+    Snake snake_movement;
+
+    while (!snake_movement.isKilledByWall(snake_pos, video_mode) && !snake_movement.isKilledByItself(snake, snake_pos)) {
+
+        // Update
+
+        update(window);
+
+        // Render
+
+        render(window);
+    }
+
+    cout << score << endl;
+    
 }
