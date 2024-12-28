7930e828d99937efd150949fcb359f127f8d2c35
MuhammadAbuBakarQureshi <152699100+MuhammadAbuBakarQureshi@users.noreply.github.com>
2024-12-28 10:00:16 +0500

    Update main.cpp

--- Snake-Glide/main.cpp
+++ Snake-Glide/main.cpp
@@ -1,29 +1,6 @@
-#include "Game/Game.h"
-#include "Button/Button.h"
 #include "Main Menu/MainMenu.h"
 
-using namespace std;
-
 int WinMain() {
-
-	//Game game;
-
-	//Button start_game("Start Game");
-	//start_game.setColors(sf::Color(20, 20, 60), sf::Color(128, 180, 255));
-	//start_game.setButtonPosition(sf::Vector2f(355, 200));
-	//start_game.setTextPosition(sf::Vector2f(440, 220));
-
-	//Button exit("Exit");
-	//exit.setColors(sf::Color(20, 20, 60), sf::Color(128, 180, 255));
-	//exit.setButtonPosition(sf::Vector2f(355, 400));
-	//exit.setTextPosition(sf::Vector2f(480, 420));
-
-	//while (game.isRunning()) {
-
-	//	game.update();
-
-	//	game.render();
-	//}
 
 	MainMenu menu;
 
